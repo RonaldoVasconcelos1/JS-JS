c9981366060211207f2a7a8fbea3d65be1d17d11
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2020-08-19 23:03:29 -0300

    Metodo Delete

--- Modulo-2-Desenvolvimento-API/Modulo-parte-3/api-my-bank/routes/accounts.js
+++ Modulo-2-Desenvolvimento-API/Modulo-parte-3/api-my-bank/routes/accounts.js
@@ -5,50 +5,60 @@ import { promises as fs } from "fs";
 const router = express.Router();
 router.post("/", async (req, res) => {
   try {
-
     let account = req.body;
     const data = await fs.readFile(global.FileName);
     const json = JSON.parse(data);
 
-
-    account = {id: json.nextId++, ...account}
+    account = { id: json.nextId++, ...account };
     json.accounts.push(account);
 
     await fs.writeFile(global.FileName, JSON.stringify(json, null, 2));
     res.send(account);
     res.end();
   } catch (err) {
-      res.status(400).send({ error: err.message});
+    res.status(400).send({ error: err.message });
   }
-
 });
 
 router.get("/", async (req, res) => {
-
-      try {
-
-        const data = await fs.readFile(global.FileName);
-        const json = JSON.parse(data);
-        delete json.nextId;
-        res.send(json);
-        
-      } catch (err) {
-          res.status(400).send({ error: err.message});
-      }
+  try {
+    const data = await fs.readFile(global.FileName);
+    const json = JSON.parse(data);
+    delete json.nextId;
+    res.send(json);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
 });
 
 router.get("/:id", async (req, res) => {
-    try {
-      const data = await fs.readFile(global.FileName);
-      const json = JSON.parse(data);
+  try {
+    const data = await fs.readFile(global.FileName);
+    const json = JSON.parse(data);
 
-      const account = json.accounts.find(
-        account => account.id === parseInt(req.params.id));
-      res.send(account); 
-      
-    } catch (err) {
-        res.status(400).send({ error: err.message});
-    }
+    const account = json.accounts.find(
+      (account) => account.id === parseInt(req.params.id)
+    );
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const data = await fs.readFile(global.FileName);
+    const json = JSON.parse(data);
+
+    json.accounts = json.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id)
+    );
+    await fs.writeFile(global.FileName, JSON.stringify(json, null, 2));
+    res.send("deletado com sucesso");
+    res.end();
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
 });
 
 export default router;
