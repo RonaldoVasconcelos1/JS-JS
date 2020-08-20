9f76277fff528b939ba2b3cb539b4c3bea5faafd
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2020-08-19 21:10:30 -0300

    commit parcial

---
+++ Modulo-2-Desenvolvimento-API/Modulo-parte-3/api-my-bank/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import accountsRouter from './routes/accounts.js';
+import { promises as fs } from 'fs';
+
+const app = express();
+app.use(express.json());
+
+
+
+app.use("/account", accountsRouter);
+
+app.listen(3030, async () => {
+    try {
+
+        await fs.readFile("accounts.json");
+        console.log("UHUUU");
+        
+    } catch (err) {
+        
+        const initialJson = {
+            nextId : 1,
+            accounts : []
+        }
+        try {
+            await fs.writeFile("accounts.json", JSON.stringify(initialJson));
+            console.log("UHUUU");
+        
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+   
+})
