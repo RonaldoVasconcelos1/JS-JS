f561061f4f5501c330005840c767a85acd233ff6
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2020-08-19 22:04:24 -0300

    implementacao do metodo POST

--- Modulo-2-Desenvolvimento-API/Modulo-parte-3/api-my-bank/routes/accounts.js
+++ Modulo-2-Desenvolvimento-API/Modulo-parte-3/api-my-bank/routes/accounts.js
@@ -1,5 +1,28 @@
-import express from 'express';
+import express from "express";
 
-const routes = express.Router();
+import { promises as fs } from "fs";
 
-export default routes;
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+
+    let account = req.body;
+    const data = await fs.readFile("accounts.json");
+    const json = JSON.parse(data);
+
+
+    account = {id: json.nextId++, ...account}
+    json.accounts.push(account);
+
+    await fs.writeFile("accounts.json", JSON.stringify(json));
+    res.send(account);
+
+    res.end();
+  } catch (err) {
+      res.status(400).send({ error: err.message});
+  }
+
+});
+
+export default router;
