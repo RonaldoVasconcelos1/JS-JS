d30ca40a9a2063a1580eaecef56b8b6ce64a7e11
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2020-08-22 12:31:33 -0300

    adicionei a validação dos campos

--- Modulo-2-Desenvolvimento-API/Modulo-parte-3/api-my-bank/routes/accounts.js
+++ Modulo-2-Desenvolvimento-API/Modulo-parte-3/api-my-bank/routes/accounts.js
@@ -6,19 +6,26 @@ const router = express.Router();
 router.post("/", async (req, res, next) => {
   try {
     let account = req.body;
+
+    if (account.balance == null && !account.name) {
+      throw new error("Name e Balance são obrigatórios");
+    }
     const data = await fs.readFile(global.FileName);
     const json = JSON.parse(data);
 
-    account = { id: json.nextId++, ...account };
+    account = {
+      id: json.nextId++,
+      balance: account.balance,
+      name: account.name,
+    };
     json.accounts.push(account);
 
     await fs.writeFile(global.FileName, JSON.stringify(json, null, 2));
     res.send(account);
-    Logger.info(`POST /account ${jsonStringify(account)}`)
+    Logger.info(`POST /account ${jsonStringify(account)}`);
     res.end();
   } catch (err) {
     next(err);
-
   }
 });
 
@@ -46,7 +53,6 @@ router.get("/:id", async (req, res, next) => {
     Logger.info("GET:ID /account");
   } catch (err) {
     next(err);
-
   }
 });
 
@@ -63,59 +69,67 @@ router.delete("/:id", async (req, res, next) => {
     Logger.info(`DELETE:ID ${req.params.id}, /account`);
   } catch (err) {
     next(err);
-
   }
 });
 
 router.put("/", async (req, res, next) => {
-
   try {
-    let account = req.params;
+
+    const account = req.body;
+
+    if(!account.id) {
+      throw new Error("Registro nao encontrado");
+    }
     const data = await fs.readFile(global.FileName);
     const json = JSON.parse(data);
+    const index = json.accounts.findIndex(a => a.id === account.id);
 
-    const index = json.accounts.findIndex(a => a.id === a.id);
+    if(index == -1) {
+      throw new Error("Registro não encontrado");
+    }
 
-    json.accounts[index] =account;
+    json.accounts[index].balance = account.balance;
+    json.accounts[index].name = account.name;
 
     await fs.writeFile(global.FileName, JSON.stringify(json, null, 2));
-
     res.send(account);
-    Logger.info(`PUT:ID ${req.params.id} /account, ${jsonStringify( account)}`);
 
-
-    
+    Logger.info(`PUT:ID /account, ${JSON.stringify(account)}`);
   } catch (err) {
-
     next(err);
-
   }
 });
 
-router.patch('/updateBalance', async(req, res) => {
-
+router.patch("/updateBalance", async (req, res) => {
   try {
+    const account = req.body;
 
-    let account = req.params;
-    const data =  await fs.readFile(global.FileName);
+    if(!account.name && account.balance === null) {
+      throw new error("Name e Balance são obrigatórios");
+    }
+    const data = await fs.readFile(global.FileName);
     const json = JSON.parse(data);
-  
-    const index = json.accounts.findIndex(ac => ac.id === ac.id);
-  
-    json.accounts[index].balance = account.balance;
-  
-    await fs.writeFile(global.FileName, JSON.stringify(json, null, 2));
-  
-    res.send(account.balance);
-    Logger.info(`PATCH:ID ${req.params.id} /account, ${jsonStringify(account)}`);
 
+    const index = json.accounts.findIndex(ac => ac.id === account.id);
+
+    json.accounts[index].balance = account.balance;
+    json.accounts[index].name = account.name;;
+
+    await fs.writeFile(global.FileName, JSON.stringify(json, null, 2));
+
+    res.send(account.balance);
+    Logger.info(
+      `PATCH:ID ${req.params.id} /account, ${jsonStringify(account)}`
+    );
   } catch (err) {
     next(err);
   }
 });
 
 router.use("/", async (err, req, res, next) => {
-  global.Logger.error(`Metodo: ${req.method}, EndPoint: ${req.baseUrl}, Error: ${err.message}`);
+  global.Logger.error(
+    `Metodo: ${req.method}, EndPoint: ${req.baseUrl}, Error: ${err.message}`
+  );
   console.log(err);
   res.status(400).send({ error: err.message });
 });
